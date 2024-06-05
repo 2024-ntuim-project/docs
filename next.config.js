348bf767aea982dc8b5dcf6a134b8af065735123
YouMingYeh <b10705052@ntu.edu.tw>
2024-06-05 19:52:29 +0800

    .

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
-const isProd = process.env.NODE_ENV === "production";
-
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-const nextConfig = {
-  basePath: process.env.BASE_PATH || "",
-  assetPrefix: process.env.ASSET_PREFIX || "",
-  reactStrictMode: true,
-  output: isProd ? "export" : "standalone",
-  distDir: "dist",
+module.exports = {
+  ...withNextra(),
   images: {
     unoptimized: true,
   },
 };
-module.exports = withNextra(nextConfig);
