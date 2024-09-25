87c17cb1e38183d83160ce43179a1d485b1c9d5a
YouMingYeh <ym911216@gmail.com>
2024-09-26 02:17:39 +0800

    Update docs

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@ const isProd = process.env.NODE_ENV === "production";
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
+  latex: true
 });
 
 const nextConfig = {
   basePath: isProd ? "/docs" : "",
-  assetPrefix: "./",
+  assetPrefix: isProd ? "https://2024-ntuim-project.github.io/docs/" : "",
   reactStrictMode: true,
   output: isProd ? "export" : "standalone",
   distDir: "dist",
